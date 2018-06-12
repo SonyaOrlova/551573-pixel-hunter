722840a41355ad551134f81603387d39de17f142
SonyaOrlova <sonya5488@yandex.ru>
2018-06-12 19:30:40 +0700

    Убирает тесты

--- gulpfile.js
+++ gulpfile.js
@@ -107,15 +107,5 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, function () {
-  return gulp
-  .src([`js/**/*.test.js`])
-  .pipe(rollup({
-    plugins: [
-      commonjs()           // Сообщает Rollup, что модули можно загружать из node_modules
-    ]}, `cjs`))            // Выходной формат тестов — `CommonJS` модуль
-  .pipe(gulp.dest(`build/test`))
-  .pipe(mocha({
-    reporter: `spec`     // Вид в котором я хочу отображать результаты тестирования
-  }));
+gulp.task(`test`, () => {
 });
