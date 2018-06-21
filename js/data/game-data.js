e1bdaad5ef287c9c2ab8408449c6b135b0d54560
SonyaOrlova <sonya5488@yandex.ru>
2018-06-21 12:50:55 +0300

    правка

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -14,23 +14,23 @@
 const pictures = {
   paintings: [
     // People
-    'https://k42.kn3.net/CF42609C8.jpg',
+    `https://k42.kn3.net/CF42609C8.jpg`,
 
     // Animals
-    'https://k42.kn3.net/D2F0370D6.jpg',
+    `https://k42.kn3.net/D2F0370D6.jpg`,
 
     // Nature
-    'https://k32.kn3.net/5C7060EC5.jpg'
+    `https://k32.kn3.net/5C7060EC5.jpg`
   ],
   photos: [
     // People
-    'http://i.imgur.com/1KegWPz.jpg',
+    `http://i.imgur.com/1KegWPz.jpg`,
 
     // Animals
-    'https://i.imgur.com/DiHM5Zb.jpg',
+    `https://i.imgur.com/DiHM5Zb.jpg`,
 
     // Nature
-    'http://i.imgur.com/DKR1HtB.jpg'
+    `http://i.imgur.com/DKR1HtB.jpg`
   ]
 };
 
